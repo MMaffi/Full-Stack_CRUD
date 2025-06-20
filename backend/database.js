a746937a65a92d4a0d37896e7c28b6eabbde1e4e
Mateus Maffi <mateusmaffi@fxsistemas.com.br>
2025-06-20 10:37:32 -0300

    add .env file to connection

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,19 @@
 import mysql from "mysql2"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "<your_user>",
-    password: "<your_password>",
-    database: "<name_of_database>"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error("Erro na conexão com o banco de dados:", err.message)
+    } else {
+        console.log("Conectado ao banco de dados com sucesso!")
+    }
 })
