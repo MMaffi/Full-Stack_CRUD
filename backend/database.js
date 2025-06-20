055e39eeb4d629f927a163239d69b5852c6b0972
MMaffi <121996649+MMaffi@users.noreply.github.com>
2025-06-20 00:04:34 -0300

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql2"
 
 export const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "cruddb"
+    user: "<your_user>",
+    password: "<your_password>",
+    database: "<name_of_database>"
 })
